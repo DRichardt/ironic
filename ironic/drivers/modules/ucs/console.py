c7bec0b60ae2eb377025d22d82b8f010e3eac91e
Fabian Wiesel <fabian.wiesel@sap.com>
2018-09-06 12:04:36 +0200

    Fixup UcsShellInaboxConsole

--- ironic/drivers/modules/ucs/console.py
+++ ironic/drivers/modules/ucs/console.py
@@ -1,9 +1,16 @@
 import os
-from oslo_log import log as logging
 
+from oslo_log import log as logging
+from oslo_utils import excutils
+
+from ironic.common import exception
 from ironic.conf import CONF
 from ironic.drivers.modules import console_utils
-from ironic.drivers.modules.ipmitool import IPMIShellinaboxConsole, _parse_driver_info, _console_pwfile_path
+from ironic.drivers.modules.ipmitool import _console_pwfile_path
+from ironic.drivers.modules.ipmitool import _parse_driver_info
+from ironic.drivers.modules.ipmitool import IPMIShellinaboxConsole
+
+from ironic_lib import utils as ironic_utils
 
 LOG = logging.getLogger(__name__)
 
@@ -25,18 +32,19 @@ class UcsShellInaboxConsole(IPMIShellinaboxConsole):
         pw_file = console_utils.make_persistent_password_file(
             path, driver_info['password'] or '\0')
         user = driver_info.get('username')
-        address = driver_info['address']
+        address = driver_info.get('address')
 
         cmd = (("/:%(uid)s:%(gid)s:HOME:" + CONF.console.ssh_command_pattern) %
                 {'uid': os.getuid(),
                  'gid': os.getgid(),
                  'pw_file': pw_file,
-                 'username': driver_info.get('username'),
-                 'address': driver_info.get('address')
+                 'username': user,
+                 'address': address
                  })
 
         try:
-            console_utils.start_shellinabox_console(driver_info['uuid'], driver_info['port'], cmd)
+            console_utils.start_shellinabox_console(driver_info['uuid'],
+                                                    driver_info['port'], cmd)
         except (exception.ConsoleError, exception.ConsoleSubprocessFailed):
             with excutils.save_and_reraise_exception():
                 ironic_utils.unlink_without_raise(path)
